c2b1f592a8dcef2152817bffb87217566fbfd465
Pedro Ventura <pedro.ventura19g@gmail.com>
2020-06-05 14:50:31 +0100

    base with some styling

---
+++ src/MemeGen.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class MemeGen extends Component {
+    constructor() {
+      super()
+      this.state ={}
+    }
+
+    render() {
+      return (
+          <h1>MEME GENERATOR SECTION</h1>)
+    }
+}
+
+export default MemeGen
